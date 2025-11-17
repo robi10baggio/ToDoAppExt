a3ef6edec3bedc44237477ca93d86190be8335d0
Haruyoshi Gonsho <robi10@outlook.jp>
2025-11-18 07:21:42 +0900

    2ns

---
+++ src/main/java/com/todo/app/form/admin/UserForm.java
@@ -0,0 +1,31 @@
+package com.todo.app.form.admin;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
+public class UserForm {
+	private Long id;
+	
+	@NotEmpty
+	@Size(max = 40, message = "{0}は{1}文字以上で入力してください。")
+	@Email(message = "{0}はメールアドレスの形式で入力してください。")
+	private String userId;
+	
+	@NotEmpty
+	@Size(max = 40, message = "{0}は{1}文字以上で入力してください。")
+	private String userName;
+	
+	@Size(min = 5, max = 40, message = "{0}は{1}文字以上{2}文字以下で入力してください。")
+	private String password;
+	
+	private Long teamId;
+	
+	private Integer role;
+	
+	@Size(min = 5, max = 40, message = "{0}は{1}文字以上{2}文字以下で入力してください。")
+	private String checkPassword;
+}
