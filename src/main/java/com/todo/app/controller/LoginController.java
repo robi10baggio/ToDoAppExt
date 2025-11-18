e08ed3fcff9bf9b99d74502b3c32cf34e1ab8fd9
Haruyoshi Gonsho <robi10@outlook.jp>
2025-11-19 08:38:43 +0900

    BUG

--- src/main/java/com/todo/app/controller/LoginController.java
+++ src/main/java/com/todo/app/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -18,6 +19,7 @@ import com.todo.app.model.Account;
 import com.todo.app.service.UserService;
 
 @Controller
+@RequestMapping("/")
 public class LoginController {
 
 	@Autowired
