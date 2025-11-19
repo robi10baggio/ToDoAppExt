21486e3feee64779947a0c2b247331ef45d9ea54
Haruyoshi Gonsho <robi10@outlook.jp>
2025-11-20 07:39:17 +0900

    バリデーションエラーを仕様に合わせた

--- src/main/java/com/todo/app/form/admin/UserForm.java
+++ src/main/java/com/todo/app/form/admin/UserForm.java
@@ -16,16 +16,16 @@ public class UserForm {
 	private String userId;
 	
 	@NotEmpty
-	@Size(max = 40, message = "{0}は{1}文字以上で入力してください。")
+	@Size(max = 40, message = "{0}は{1}文字以内で入力してください。")
 	private String userName;
 	
-	@Size(max = 40, min = 5, message = "{0}は{1}文字以上{2}文字以下で入力してください。")
+	@Size(min = 5, max = 40, message = "{0}は{1}文字以内{2}文字以上で入力してください。")
 	private String password;
 	
 	private Long teamId;
 	
 	private Integer role;
 	
-	@Size(max = 40, min = 5,  message = "{0}は{1}文字以上{2}文字以下で入力してください。")
+	@Size(min = 5, max = 40, message = "{0}は{1}文字以内{2}文字以上で入力してください。")
 	private String checkPassword;
 }
